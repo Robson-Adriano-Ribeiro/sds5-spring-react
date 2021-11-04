6eaabf92c3c70e16c1eee9c1724337a69fa72c24
Robson <robson.adriano@outlook.com.br>
2021-11-03 21:10:37 -0300

    feat: add component footer

--- sds5-react/src/App.tsx
+++ sds5-react/src/App.tsx
@@ -1,12 +1,14 @@
+import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 
 function App() {
   return (
     <>
-      <NavBar></NavBar>
+      <NavBar />
       <div className="container">
         <h1 className="text-primary">session01</h1>
       </div>
+      <Footer />
     </>
   );
 }
